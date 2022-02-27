d52db146b739e33638d50b633d2665a75365ae7a
Nusera <nusera@zipcoders-MacBook-Pro.local>
2022-02-26 20:19:44 -0500

    added basic operating fucntions

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import sun.applet.Main;
+
 /**
  * Created by leon on 2/9/18.
  */
@@ -9,9 +11,53 @@ public class MainApplication {
         String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
         Double d = Console.getDoubleInput("Enter a double.");
+        Double d2 = Console.getDoubleInput("Enter a double.");
 
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+        Console.println("The user input %s as a d", d2);
     }
+    public static class Basic extends MainApplication {
+
+        public static double add(double d, double d2) {
+            return d + d2;
+        }
+        public static double sub(double d, double d2) {
+            return d - d2;
+        }
+        public static double mul(double d, double d2) {
+            return d * d2;
+        }
+        public static double div(double d, double d2) {
+            return d / d2;
+        }
+        public static double sq(double d) {
+            return d * d;
+        }
+        public static double sqrt(double d) {
+            return java.lang.Math.sqrt(d);
+        }
+
+        public static double pow(double d, double d2) {
+            return java.lang.Math.pow(d, d2);
+        }
+
+        public static double inexpo(double d) {
+            return 1 / d;
+        }
+        public static double sswitch(double d) {
+            return d * -1;
+        }
+
+    }
+
+    public static class Scientific extends MainApplication {
+
+        public static double sin(double d) {
+            return java.lang.Math.sin(d);
+        }
+    }
+
 }
+
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,105 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static jdk.nashorn.internal.objects.Global.Infinity;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    MainApplication.Basic basic = new MainApplication.Basic();
+
+    @Test
+    public void addTest(){
+
+        double d = 4.1,
+                d2 = 10;
+        double expected = 14.1;
+        double actual = basic.add(d, d2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void subTest(){
+        double d = 4,
+                d2 = 10;
+        double expected = -6;
+        double actual = basic.sub(d, d2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mulTest(){
+        double d = 4,
+                d2 = 10;
+        double expected = 40;
+        double actual = basic.mul(d, d2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divTest(){
+        double d = 1,
+                d2 = 4;
+        double expected = .25;
+        double actual = basic.div(d, d2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sqTest(){
+        double d = 3;
+        double expected = 9;
+        double actual = basic.sq(d);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sqrtTest(){
+        double d = 36;
+        double expected = 6;
+        double actual = basic.sqrt(d);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void powTest(){
+        double d = 6,
+                d2 = 4;
+        double expected = 1296;
+        double actual = basic.pow(d, d2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inexpoTest(){
+        double d = 3;
+        double expected = 0.3333333333333333 ;
+        double actual = basic.inexpo(d);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sswitchTest(){
+        double d = -3;
+        double expected = 3 ;
+        double actual = basic.sswitch(d);
+        Assert.assertEquals(expected, actual);
+    }
+
+    MainApplication.Scientific scientific = new MainApplication.Scientific();
+
+    @Test
+    public void sinTest(){
+        double d = 3;
+        double expected = 0.1411200080598672 ;
+        double actual = scientific.sin(d);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
 }
