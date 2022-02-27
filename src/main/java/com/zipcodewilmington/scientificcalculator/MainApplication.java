d9c5e6c19274b8d17359ef6829c7daf1ff809126
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-02-27 01:40:29 -0500

    added additional method calls

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,8 +4,9 @@ import sun.applet.Main;
 
 import java.math.BigInteger;
 
-import static com.zipcodewilmington.scientificcalculator.Console.Basic.add;
-import static com.zipcodewilmington.scientificcalculator.Console.Basic.sub;
+import static com.zipcodewilmington.scientificcalculator.Console.Basic.*;
+import static java.lang.Double.NaN;
+import static jdk.nashorn.internal.objects.Global.Infinity;
 
 /**
  * Created by leon on 2/9/18.
@@ -18,10 +19,10 @@ public class MainApplication {
         Double d = Console.getDoubleInput("Enter a double.");
         Double d2 = Console.getDouble2Input("Enter a double.");
 
-        Console.println("The user input %s as a command", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
-        Console.println("The user input %s as a d2", d2);
+        // Console.println("The user input %s as a command", s);
+        // Console.println("The user input %s as a integer", i);
+        // Console.println("The user input %s as a d", d);
+        // Console.println("The user input %s as a d2", d2);
 
         double displayNum = 0;
         BigInteger displayNumBig = BigInteger.valueOf(0);
@@ -29,12 +30,29 @@ public class MainApplication {
 
         if(s.equals("add")){
             displayNum = add(d,d2);
-        } else if (s.equals("subtract")){
-            displayNum = sub(d,d2);
+        } else if (s.equals("subtract")) {
+            displayNum = sub(d, d2);
+        } else if (s.equals("multiply")) {
+            displayNum = mul(d, d2);
+        } else if (s.equals("divide")) {
+            displayNum = div(d, d2);
+        } else if (s.equals("square")) {
+            displayNum = sq(d);
+        } else if (s.equals("square root")) {
+            displayNum = sqrt(d);
+        } else if (s.equals("exponent")) {
+            displayNum = pow(d,d2);
         } else {
             displayNum = 7;
         }
-        System.out.println(displayNum);
+
+        if(displayNum == Infinity) {
+            System.out.println("err");
+        } else if (Double.isNaN(displayNum)) {
+            System.out.println("err");
+        } else {
+            System.out.println(displayNum);
+        }
     }
 
 
